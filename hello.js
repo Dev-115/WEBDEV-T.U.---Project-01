9544625108848af6a1dcd88ac4509b91b5363e6c
HenrickL <markhenrick.linsangan@gmail.com>
2022-08-10 16:29:58 +0800

    this is my branch

--- hello.js
+++ hello.js
@@ -3,3 +3,4 @@ console.log('git is installed!');
 console.log('mark is here!'); //add line console.log(yourname is here)
 console.log('Genesis was here!'); //add line console.log(yourname is here)
 console.log('Gab was here!'); //add line console.log(yourname is here)
+console.log('mark\'s branch'); 
