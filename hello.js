5fdef9d2818412f38a0e503969bcf5c86680fbcd
Gab Sanchez <brylesanchez15@gmail.com>
2022-08-10 16:20:29 +0800

    added 'gab was here' in hello.js

--- hello.js
+++ hello.js
@@ -2,3 +2,4 @@ console.log('Node is installed!');
 console.log('git is installed!');
 console.log('mark is here!'); //add line console.log(yourname is here)
 console.log('Genesis was here!'); //add line console.log(yourname is here)
+console.log('Gab was here!'); //add line console.log(yourname is here)
