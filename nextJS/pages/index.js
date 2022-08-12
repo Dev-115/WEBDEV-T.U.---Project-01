967f80730340ef6c2da11dbb03913aea8be24c32
HenrickL <markhenrick.linsangan@gmail.com>
2022-08-13 03:42:22 +0800

    some nextJS templete addtions

--- nextJS/pages/index.js
+++ nextJS/pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
-
+import Link from 'next/link';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
 export default function Home() {
   console.log('Node is installed!');
 console.log('git is installed!');
@@ -21,49 +23,20 @@ console.log('mark\'s branch');
         </h1>
 
         <p className="description">
-          Powered by  <code>React/Next.js</code>
+         Genesis
+         Gab
+         Albert
+         Mark
         </p>
+        <h4 className="title">
+         Read <Link href="/login">this page!</Link>
+        </h4>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
+      Powered by  <code>React/Next.js</code>
       </footer>
 
       <style jsx>{`
